b55aa5a75f4af712b9c9f45e5d694939479bc843
Štěpán Beneš <34740754+Midoriii@users.noreply.github.com>
2020-08-09 10:35:19 +0200

    Add files via upload

---
+++ src/main/java/com/apprehension/midoricave/MidoriCaveApplication.java
@@ -0,0 +1,13 @@
+package com.apprehension.midoricave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidoriCaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidoriCaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apprehension/midoricave/MidoriCaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apprehension.midoricave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidoriCaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
