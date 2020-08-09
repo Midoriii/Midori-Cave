ddbdade1bf9eaa2ed3245fc394684b1b9b3def93
Midoriii <stephen.Team24@gmail.com>
2020-08-09 16:57:54 +0200

    Extended get mapping

--- src/main/java/com/apprehension/midoricave/Controller/BottleRestController.java
+++ src/main/java/com/apprehension/midoricave/Controller/BottleRestController.java
@@ -3,6 +3,7 @@ package com.apprehension.midoricave.Controller;
 import java.util.Collection;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,8 @@ import com.apprehension.midoricave.Service.Impl.BottleServiceImpl;
  *
  */
 
+//TODO switch to DTOs when rdy
+
 @RestController
 @RequestMapping("/bottles")
 public class BottleRestController {
@@ -28,9 +31,34 @@ public class BottleRestController {
 		this.service = srv;
 	}
 	
+	@GetMapping("/all/brand/{brand}")
+	public Collection<Bottle> allByBrand(@PathVariable String brand){
+		return service.getBottlesByBrand(brand);
+	}
+	
+	@GetMapping("/all/type/{type}")
+	public Collection<Bottle> allByType(@PathVariable String type){
+		return service.getBottlesByType(type);
+	}
+	
+	@GetMapping("/all/name/{name}")
+	public Collection<Bottle> allByName(@PathVariable String name){
+		return service.getBottleByName(name);
+	}
+	
+	@GetMapping("/all/country/{country}")
+	public Collection<Bottle> allByCountry(@PathVariable String country){
+		return service.getBottlesByCountry(country);
+	}
+	
 	@GetMapping("/all")
 	public Collection<Bottle> all(){
 		return service.getAllBottles();
+	}
+	
+	@GetMapping("/{id}")
+	public Bottle getSingleBottle(@PathVariable Long Id) {
+		return service.getBottleById(Id);
 	}
 	
 	
