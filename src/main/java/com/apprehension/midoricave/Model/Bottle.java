eb316d05facb1b63b9ca746eeb1ae14af6479a83
Midoriii <stephen.Team24@gmail.com>
2020-08-09 12:22:15 +0200

    Added DB url and Bottle model

--- src/main/java/com/apprehension/midoricave/Model/Bottle.java
+++ src/main/java/com/apprehension/midoricave/Model/Bottle.java
@@ -1,7 +1,17 @@
 package com.apprehension.midoricave.Model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,5 +33,45 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "Bottles")
 public class Bottle {
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long Id;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String name;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String type;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String volume;
+	
+	@NotNull
+	@Column(nullable = false)
+	private float percentage;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String brand;
+	
+	@Column(name = "country_acronym" , length = 3)
+    @Size(max = 3)
+	private String country;
+	
+	// A Link to a shop where it can be bought
+	@Column
+	@URL
+	private String link;
+	
+	@Column
+	private String description;
+	
+	// A link to the url of an image of the bottle
+	@Column
+	@URL
+	private String img_url;
+	
 }
